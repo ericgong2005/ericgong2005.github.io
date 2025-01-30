010c11fb97d6267b0c496ecbadac3e0297dbcbe5
Eric Gong <ericgong2005@gmail.com>
2025-01-29 22:22:40 -0500

    add sitemap generation script

---
+++ tools/image_finder.py
@@ -0,0 +1,71 @@
+'''
+The following script is used for two purposes:
+First, to generate a report of website image sizes, so that I may find and reduce large images
+Second, to generate an image sitemap to support Google Search
+'''
+
+from pathlib import Path
+
+IMAGE_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp'}
+
+WEBSITE_URL = "https://ericgong2005.github.io/"
+
+WEBSITE_DIRECTORY = Path(__file__).parent.parent / "docs"
+
+REPORT_FILE = Path(__file__).parent / "Image_Report.txt"
+
+SITEMAP_FILE = Path(__file__).parent.parent / "docs" / "image_sitemap.xml"
+
+def xml_header():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+"""
+
+def xml_image_entry(url):
+    return f"""    <url>
+        <loc>{WEBSITE_URL}{url}</loc>
+        <changefreq>daily</changefreq>
+        <image:image>
+            <image:loc>{WEBSITE_URL}{url}</image:loc>
+        </image:image>
+    </url>
+"""
+
+def find_images():
+    image_list = []
+    for image in WEBSITE_DIRECTORY.rglob("*"):
+        # Do not check the _site directory
+        if "_site" not in image.parts and image.suffix.lower() in IMAGE_EXTENSIONS:
+            file_path = image.relative_to(WEBSITE_DIRECTORY)  
+            file_size = image.stat().st_size
+
+            #kB size
+            display_size = file_size / 1024
+            size_units = "KB"
+            
+            if display_size > 1024:
+                display_size = display_size / 1024
+                size_units = "MB"
+
+            image_list.append({"Path" : file_path, "Display": f"Size: {display_size:.3f} {size_units}", "Size": file_size})
+    image_list = sorted(image_list, key=lambda x: x["Size"], reverse=True)
+
+    with open(REPORT_FILE, 'w') as f:
+        f.write("Image File Report\n")
+        f.write("=" * 50 + "\n")
+        for image in image_list:
+            f.write(f"Path: {image['Path']}\n")
+            f.write(image["Display"] + "\n")
+            f.write("-" * 50 + "\n")
+    
+    with open(SITEMAP_FILE, 'w') as f:
+        f.write(xml_header())
+        for image in image_list:
+            f.write(xml_image_entry(image["Path"]))
+        f.write('</urlset>')
+    
+find_images()
